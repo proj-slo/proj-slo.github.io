1dc1743f968c45960d7e2ee91771cb3a9e1f4010
Ali Madooei <alimadooei@gmail.com>
2025-01-21 08:42:28 -0500

    refactor App component to use Layout with resizable panels and remove unused state and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import { useState, useEffect } from "react";
-import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
+import { useEffect } from "react";
+import Layout from "@/components/layout";
 import { useTheme } from "@/hooks/use-theme";
-import ThemeToggle from "@/components/theme-toggle";
-import AboutDialog from "@/components/about-dialog";
 
 function App() {
-  const [text, setText] = useState("");
   const { theme } = useTheme();
-
-  const check = () => {
-    console.log("Checking text...");
-  };
 
   useEffect(() => {
     const root = window.document.documentElement;
@@ -29,39 +21,30 @@ function App() {
     root.classList.add(theme);
   }, [theme]);
 
-  return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="flex flex-col w-full md:w-3/4 lg:w-2/3 xl:w-1/2 p-4 gap-3">
-        <div className="flex flex-col gap-1 mb-1 sm:mb-2 md:mb-3 lg:mb-4">
-          <div className="flex justify-between items-center">
-            <h1 className="text-xl md:text-2xl lg:text-3xl font-semibold">
-              Simple Grammarly
-            </h1>
-            <div className="flex items-center justify-end gap-1">
-              <AboutDialog />
-              <ThemeToggle />
-            </div>
-          </div>
-          <p className="text-sm text-muted-foreground">
-            Check your text for grammar and spelling mistakes.
-          </p>
-        </div>
-        <Textarea
-          className="md:text-xl"
-          placeholder="Type or paste (⌘+V) your text here"
-          rows={14}
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <Button
-          onClick={() => {
-            check();
-          }}
-        >
-          Check
-        </Button>
-      </div>
+  const left = (
+    <div className="bg-blue-100 dark:bg-blue-900">
+      <h1>Left Panel</h1>
     </div>
+  );
+
+  const middle = (
+    <div className="bg-green-100 dark:bg-green-900">
+      <h1>Middle Panel</h1>
+    </div>
+  );
+
+  const right = (
+    <div className="bg-yellow-100 dark:bg-yellow-900">
+      <h1>Right Panel</h1>
+    </div>
+  );
+
+  return (
+    <Layout
+      leftPanelContent={left}
+      middlePanelContent={middle}
+      rightPanelContent={right}
+    />
   );
 }
 
