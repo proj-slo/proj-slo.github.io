8923c654723d8a796f83c0bf7ce3dbb65b5f321e
Ali Madooei <alimadooei@gmail.com>
2025-01-21 16:10:48 -0500

    add border to header component for improved visual separation and adjust font weight and icon size

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,19 +7,19 @@ const DEBUG = false;
 const Header: React.FC = () => {
   return (
     <header
-      className={cn("flex items-center justify-between gap-1 w-full py-1", {
+      className={cn("flex items-center justify-between gap-1 w-full py-1 border-b", {
         "border-2 border-green-500": DEBUG,
       })}
     >
       <div
         className={cn(
-          "flex items-center justify-start w-full font-semibold text-lg",
+          "flex items-center justify-start w-full font-light text-muted-foreground",
           {
             "border-2 border-blue-500": DEBUG,
           },
         )}
       >
-        <Bot className="w-6 h-6 mr-2" />
+        <Bot className="w-5 h-5 mr-1" />
         <span className="truncate">Simple ChatGPT</span>
       </div>
       <ThemeToggle />
