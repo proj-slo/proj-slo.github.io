f05677a8b47d36a1ab4ede6c523fbf4eda3a0096
Ali Madooei <alimadooei@gmail.com>
2025-01-22 09:37:47 -0500

    wrap App component with TooltipProvider for enhanced tooltip functionality

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "@/App.tsx";
 import "@/index.css";
 import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-    <Toaster richColors />
+    <TooltipProvider>
+      <App />
+      <Toaster richColors />
+    </TooltipProvider>
   </StrictMode>,
 );
