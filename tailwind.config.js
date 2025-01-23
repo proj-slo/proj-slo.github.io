052f3f07f16da8cb1251f2028431f4e8ef6c9003
Ali Madooei <alimadooei@gmail.com>
2025-01-22 20:08:22 -0500

    feat: add tailwind-scrollbar dependency and configure in Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -53,5 +53,9 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/typography"),
+    require("tailwind-scrollbar")({ nocompatible: true }),
+  ],
 };
